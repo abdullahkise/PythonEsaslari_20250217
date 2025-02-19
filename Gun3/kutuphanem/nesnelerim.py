94cef45ecb3d974e32ead7507ea9aa5123385090
Abdullah Kise <abdullahkise@hotmail.com>
2025-02-19 16:31:44 +0300

    gun3

---
+++ Gun3/kutuphanem/nesnelerim.py
@@ -0,0 +1,8 @@
+#yukarıdaki komut ile bu hücre içerisini belirtilen dosyaya yazarız.
+#$ ile notebook içindeki değişkenlere erişmiş oluyoruz.
+def c2f(c):
+    return 1.8*c+32
+
+#-----------------
+yazar = "Abdullah Kise"
+mail = "abdullahkise@hotmail.com"
